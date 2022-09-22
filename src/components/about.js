7d09761b504bd517d62a2b27641546db086f0bba
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-09-22 18:05:06 +0300

    Mini project

---
+++ src/components/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About ({ 
+    image="https://via.placeholder.com/215",alt,about}){
+        return(
+            <aside>
+                <img src={image} alt="blog logo"/>
+                <>
+                <p>{about}</p>
+                {/* <p>{about}</p> */}
+                </>
+            </aside>
+        )
+    }
+    
+    export default About
+    
